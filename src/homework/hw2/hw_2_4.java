188b4e594dbf2547956a470ebf8023abf33f95f8
DiezNote <dieznote@gmail.com>
2016-11-08 14:40:32 +0200

    Initial commit

--- src/homework/hw2/hw_2_4.java
+++ src/homework/hw2/hw_2_4.java
@@ -1,7 +1,19 @@
 package homework.hw2;
 
+import java.util.Scanner;
+
 /**
  * проверка введенного целого числа на четность
  */
 public class hw_2_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chislo = 0;
+        System.out.println("Введите целое число и нажмите <Enter>");
+        chislo = sc.nextInt();
+        if((chislo % 2) == 0)
+            System.out.print("Число "+chislo+" четное");
+        else
+            System.out.print("Число "+chislo+" нечетное");
+    }
 }
