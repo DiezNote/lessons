f95efe19bc1eeee6ed403c8cf696a3b10db6e2ef
DiezNote <dieznote@gmail.com>
2016-11-08 14:09:45 +0200

    Initial commit

--- src/homework/hw2/hw_2_1.java
+++ src/homework/hw2/hw_2_1.java
@@ -9,7 +9,6 @@ public class hw_2_1 {
     public static void main(String args[]){
         int a = 0;
         int b = 0;
-        int c = 0;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("a:b = c");
@@ -17,7 +16,6 @@ public class hw_2_1 {
         a = sc.nextInt();
         System.out.print("введите b:");
         b = sc.nextInt();
-
 
         if(b == 0){
             System.out.println("Ошибка ввода! b не может быть равен 0");
--- src/homework/hw2/hw_2_3.java
+++ src/homework/hw2/hw_2_3.java
@@ -1,7 +1,27 @@
 package homework.hw2;
 
+import java.util.Scanner;
+
 /**
  * написать программу для вычисления стоимости покупки с учетом скидки
  */
 public class hw_2_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double price = 0;
+        double price_after_sale = 0;
+        System.out.print("Введите стоимость покупки :");
+        price = sc.nextDouble();
+        double sale_3 = 0.03 * price;
+        double sale_5 = 0.05 * price;
+
+        if(price > 1000) {
+            price_after_sale = price - sale_5;
+            System.out.println("Стоимость покупки со скидкой в 5% равна "+price_after_sale+" руб.");
+        }else if(price > 500){
+            price_after_sale = price - sale_3;
+            System.out.println("Стоимость покупки со скидкой в 3% равна "+price_after_sale+" руб.");
+        }
+    }
 }
+
