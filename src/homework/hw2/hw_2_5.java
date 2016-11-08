e01f2f1283a4743b095a3459e8f86784c9b5a38a
DiezNote <dieznote@gmail.com>
2016-11-08 14:48:37 +0200

    Initial commit

--- src/homework/hw2/hw_2_5.java
+++ src/homework/hw2/hw_2_5.java
@@ -1,7 +1,19 @@
 package homework.hw2;
 
+import java.util.Scanner;
+
 /**
  * проверка делимости введенного числа с клавиатуры на 3
  */
 public class hw_2_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chislo = 0;
+        System.out.print("Введите число: ");
+        chislo = sc.nextInt();
+        if ((chislo % 3) == 0)
+            System.out.print("Число "+chislo+" делиться нацело на 3");
+        else
+            System.out.print("Число "+chislo+" не делиться нацело на 3");
+    }
 }
