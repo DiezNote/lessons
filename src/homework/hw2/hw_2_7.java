580f35b30b32f6aa6d670a8d46328159e65d98a5
DiezNote <dieznote@gmail.com>
2016-11-08 15:50:14 +0200

    Initial commit

--- src/homework/hw2/hw_2_7.java
+++ src/homework/hw2/hw_2_7.java
@@ -1,7 +1,29 @@
 package homework.hw2;
 
+import java.util.Scanner;
+
 /**
  * трехзначное число
  */
 public class hw_2_7 {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int chislo[] = new int[3];
+        chislo[0] = 1;
+        chislo[1] = 2;
+        chislo[2] = 3;
+        System.out.println("Число 123");
+        if (chislo[0]>chislo[2])
+            System.out.println("первая цифра больше");
+        else
+            System.out.println("поседняя цифра больше");
+        if (chislo[0]>chislo[1])
+            System.out.println("первая цифра больше");
+        else
+            System.out.println("вторая цифра больше");
+        if (chislo[1]>chislo[2])
+            System.out.println("вторая цифра больше");
+        else
+            System.out.println("поседняя цифра больше");
+        }
+    }
